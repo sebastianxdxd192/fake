b2505ae8f50161c7a3343c891a8acd2b51fa54f7
sebastianxdxd192 <69281564+sebastianxdxd192@users.noreply.github.com>
2020-08-18 10:56:03 -0500

    Segunda Version 2.0
    Version 2.0

--- BotFitness-src/Bots/EchoBot.cs
+++ BotFitness-src/Bots/EchoBot.cs
@@ -19,7 +19,7 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var welcomeText = "Hola y Bienvenido!";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
